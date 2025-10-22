b64ec0cdcedddb8b3ad69389fd7a1ed635815b9f
zinchuknadia <nadiia.zinchuk.oi.2023@lpnu.ua>
2025-10-22 18:41:34 +0300

    change input validation

--- src/main/java/com/ToAndFro/client/CLIMenu.java
+++ src/main/java/com/ToAndFro/client/CLIMenu.java
@@ -6,59 +6,55 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class CLIMenu {
     private static final Logger LOGGER = LoggerFactory.getLogger(CLIMenu.class);
     private static final Scanner scanner = new Scanner(System.in);
 
     private static final String INPUT_PATTERN = "^[1-9]$";
-    private static final int EXIT_KEY = 9;
-    private static final int WRONG_INPUT_KEY = -1;
+    private static final String EXIT_KEY = "0";
 
-    private Map<Integer, MenuCommand> menuItems = Map.of(
-            1, new LoginCommand(),
-            2, new ListItemsCommand(),
-            3, new ViewItemCommand(),
-            4, new BuyItemCommand(),
-            5, new ProfileCommand(),
-            6, new MyItemsCommand(),
-            7, new AddItemCommand(),
-            8, new RemoveItemCommand()
+    private Map<String, MenuCommand> menuItems = Map.of(
+            "1", new LoginCommand(),
+            "2", new ListItemsCommand(),
+            "3", new ViewItemCommand(),
+            "4", new BuyItemCommand(),
+            "5", new ProfileCommand(),
+            "6", new MyItemsCommand(),
+            "7", new AddItemCommand(),
+            "8", new RemoveItemCommand()
     );
 
     public void run() {
         boolean isRunnable = true;
         while (isRunnable) {
             showMenu();
-            int key = readMenuChoice(scanner);
-            if (key == EXIT_KEY) {
+            String key = readMenuChoice();
+            Set<String> mapKeys = menuItems.keySet();
+            if (mapKeys.contains(key)) {
+                executeCommand(key);
+            } else if (key.equals(EXIT_KEY)) {
                 isRunnable = false;
                 LOGGER.info("User exited");
-            } else if (key != WRONG_INPUT_KEY) {
-                executeCommand(key, scanner);
+            } else {
+                System.out.println("Invalid input");
+                LOGGER.warn("Invalid input: {} is not in {}", key, INPUT_PATTERN);
             }
         }
     }
 
-    private int readMenuChoice(Scanner scanner) {
+    private String readMenuChoice() {
         System.out.print("Enter your choice: ");
-        String input = scanner.nextLine();
-        boolean isValid = input.matches(INPUT_PATTERN);
-        if (isValid) {
-            return Integer.parseInt(input);
-        } else {
-            System.out.println("Invalid input");
-            LOGGER.warn("Invalid input: {} is not in {}", input, INPUT_PATTERN);
-            return WRONG_INPUT_KEY;
-        }
+        return scanner.nextLine().trim();
     }
 
-    private void executeCommand(int key, Scanner scanner) {
+    private void executeCommand(String key) {
         menuItems.get(key).execute();
-        pressEnterToContinue(scanner);
+        pressEnterToContinue();
     }
 
-    private void pressEnterToContinue(Scanner scanner) {
+    private void pressEnterToContinue() {
         System.out.println("Press enter to continue");
         scanner.nextLine();
     }
