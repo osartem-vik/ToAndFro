9952ec3422c4271381fa1718d5b8791991a18eb5
”Artem <26and06@gmail.com>
2025-10-23 13:57:24 +0300

    changing logger to LOGGER

--- src/main/java/com/ToAndFro/configs/JdbcConnectionFactory.java
+++ src/main/java/com/ToAndFro/configs/JdbcConnectionFactory.java
@@ -8,19 +8,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JdbcConnectionFactory {
-    private final static Logger logger = LoggerFactory.getLogger(JdbcConnectionFactory.class);
+    private final static Logger LOGGER = LoggerFactory.getLogger(JdbcConnectionFactory.class);
     private final static String URL = ConfigLoader.getDBUrl();
     private final static String USERNAME = ConfigLoader.getDBUserName();
     private final static String PASSWORD = ConfigLoader.getDBPassword();
     private final static String DRIVER = ConfigLoader.getDBDriver();
 
     public static Connection getConnection() throws SQLException {
-        logger.debug("Attempting to create DB connection to: {}", URL);
+        LOGGER.debug("Attempting to create DB connection to: {}", URL);
         Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         if (connection != null) {
-            logger.debug("DB connection established successfully");
+            LOGGER.debug("DB connection established successfully");
         } else {
-            logger.warn("Failed to establish DB connection");
+            LOGGER.warn("Failed to establish DB connection");
         }
         return connection;
     }
