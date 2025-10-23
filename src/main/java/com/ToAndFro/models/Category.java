b99f30477935cd9326c7958f64440fc3ff4c5041
Andriy Tsomko <andriytsomkoo@gmail.com>
2025-10-23 22:18:26 +0300

    made changes

--- src/main/java/com/ToAndFro/models/Category.java
+++ src/main/java/com/ToAndFro/models/Category.java
@@ -1,22 +1,12 @@
 package com.ToAndFro.models;
 
 public enum Category {
-    TRANSPORT("Транспорт"),
-    REAL_ESTATE("Нерухомість"),
-    ELECTRONICS("Електроніка"),
-    HOME_GARDEN("Дім і сад"),
-    JOB("Робота"),
-    SERVICES("Послуги"),
-    FASHION("Мода"),
-    ANIMALS("Тварини");
-
-    private final String ukrainianName;
-
-    Category(String ukrainianName) {
-        this.ukrainianName = ukrainianName;
-    }
-
-    public String getUkrainianName() {
-        return ukrainianName;
-    }
+    TRANSPORT,
+    REAL_ESTATE,
+    ELECTRONICS,
+    HOME_GARDEN,
+    JOB,
+    SERVICES,
+    FASHION,
+    ANIMALS;
 }
