b62035bc4dce870af141003eaa51aef8eeba47bb
Andriy Tsomko <andriytsomkoo@gmail.com>
2025-10-23 20:16:03 +0300

    Refactor Category model and introduce Category enum

--- src/main/java/com/ToAndFro/models/Category.java
+++ src/main/java/com/ToAndFro/models/Category.java
@@ -1,19 +1,42 @@
 package com.ToAndFro.models;
 
-
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.Setter;
-import lombok.NoArgsConstructor;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 @Getter
-@Setter
-@EqualsAndHashCode
 @AllArgsConstructor
-@NoArgsConstructor
-public class Category {
-    private Long id;
-    private String name;
-    private Long parentId;
+public enum Category {
+    ELECTRONICS(1, null, "electronics"),
+    CLOTHING(2, null, "clothing"),
+    BOOKS(3, null, "books"),
+
+    SMARTPHONES(5, 1, "smartphones"),
+    LAPTOPS(6, 1, "laptops"),
+    TV(7, 1, "tv"),
+    AUDIO(8, 1, "audio"),
+
+    MEN_CLOTHING(9, 2, "men-clothing"),
+    WOMEN_CLOTHING(10, 2, "women-clothing"),
+    SHOES(11, 2, "shoes"),
+    ACCESSORIES(12, 2, "accessories"),
+
+    FICTION(13, 3, "fiction"),
+    SCIENCE_BOOKS(14, 3, "science"),
+    CHILDREN_BOOKS(15, 3, "children-books"),
+    EDUCATIONAL(16, 3, "educational");
+
+    private final int id;
+    private final Integer parentId;
+    private final String name;
+
+
+    public static Optional<Category> findById(int id) {
+        return Arrays.stream(values())
+                .filter(category -> category.id == id)
+                .findFirst();
+    }
+
 }
