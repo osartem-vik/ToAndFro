6cc87150d0e81196cd60919e7cc632c534d11572
”Artem <26and06@gmail.com>
2025-10-23 17:51:12 +0300

    added missed {}

--- src/main/java/com/ToAndFro/configs/ConfigLoader.java
+++ src/main/java/com/ToAndFro/configs/ConfigLoader.java
@@ -15,11 +15,12 @@ public class ConfigLoader {
     private final static String PATH_OF_PROPERTIES = "app.properties";
     private static final ConfigLoader INSTANCE = new ConfigLoader();
 
-    private ConfigLoader(){
+    private ConfigLoader() {
         this.properties = new Properties();
         try (InputStream input = ConfigLoader.class.getClassLoader().getResourceAsStream(PATH_OF_PROPERTIES)) {
-            if (input == null)
+            if (input == null) {
                 throw new ConfigFileNotFoundException("Unable to find a file with properties");
+            }
             properties.load(input);
             LOGGER.info("Configuration loaded successfully");
         } catch (IOException ex) {
@@ -27,19 +28,24 @@ public class ConfigLoader {
             new ConfigLoadingException("Error loading config", ex);
         }
     }
-    public static ConfigLoader getInstance(){
+
+    public static ConfigLoader getInstance() {
         return INSTANCE;
     }
-    public static String getDBUrl(){
+
+    public static String getDBUrl() {
         return properties.getProperty("db.url");
     }
-    public static  String getDBUserName(){
+
+    public static String getDBUserName() {
         return properties.getProperty("db.username");
     }
-    public static String getDBPassword(){
+
+    public static String getDBPassword() {
         return properties.getProperty("db.password");
     }
-    public static String getDBDriver(){
+
+    public static String getDBDriver() {
         return properties.getProperty("db.driver");
     }
 }
