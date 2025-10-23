35115e00b9b97bdd5a2e094dba1c3bde7e3bd02a
”Artem <26and06@gmail.com>
2025-10-23 17:52:10 +0300

    created a singlton

--- src/main/java/com/ToAndFro/configs/JdbcConnectionFactory.java
+++ src/main/java/com/ToAndFro/configs/JdbcConnectionFactory.java
@@ -13,14 +13,21 @@ public class JdbcConnectionFactory {
     private final static String USERNAME = ConfigLoader.getDBUserName();
     private final static String PASSWORD = ConfigLoader.getDBPassword();
     private final static String DRIVER = ConfigLoader.getDBDriver();
+    private static volatile Connection connection;
 
     public static Connection getConnection() throws SQLException {
-        LOGGER.debug("Attempting to create DB connection to: {}", URL);
-        Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        if (connection != null) {
-            LOGGER.debug("DB connection established successfully");
-        } else {
-            LOGGER.warn("Failed to establish DB connection");
+        if (connection == null || connection.isClosed()) {
+            synchronized (JdbcConnectionFactory.class) {
+                if (connection == null || connection.isClosed())
+
+                    LOGGER.debug("Attempting to create DB connection to: {}", URL);
+                Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                if (connection != null) {
+                    LOGGER.debug("DB connection established successfully");
+                } else {
+                    LOGGER.warn("Failed to establish DB connection");
+                }
+            }
         }
         return connection;
     }
