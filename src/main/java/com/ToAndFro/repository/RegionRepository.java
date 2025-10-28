586d897c9d9493861cbd8d4c2139f49597b7ce16
zinchuknadia <nadiia.zinchuk.oi.2023@lpnu.ua>
2025-10-28 20:18:19 +0200

    add update(region) method to repository

--- src/main/java/com/ToAndFro/repository/RegionRepository.java
+++ src/main/java/com/ToAndFro/repository/RegionRepository.java
@@ -13,6 +13,9 @@ import java.sql.SQLException;
 public class RegionRepository {
     Logger LOGGER = LoggerFactory.getLogger(RegionRepository.class);
     private final String SAVE_REGION_QUERY = "INSERT INTO region (name) VALUES (?)";
+    private final String UPDATE_REGION_QUERY = "UPDATE region SET name = ? WHERE id = ?";
+
+    private final int noChangedRows = 0;
 
     public void setRegionParams(Region region, PreparedStatement statement) throws SQLException {
         statement.setString(1, region.getName());
@@ -22,15 +25,34 @@ public class RegionRepository {
         try(Connection connection = JDBCConnectionFactory.getInstance().getConnection()){
             PreparedStatement preparedStatement = connection.prepareStatement(SAVE_REGION_QUERY);
             setRegionParams(region, preparedStatement);
+
             int res = preparedStatement.executeUpdate();
-            if(res == 0) {
+            if(res == noChangedRows) {
                 LOGGER.error("Failed saving region");
                 throw new RegionSqlException("Failed saving region");
             }
+            LOGGER.info("Region saved: {} rows affected", res);
         } catch (SQLException e) {
             LOGGER.error("Error saving region: {}", e.getMessage());
             throw new RegionSqlException("Error saving region", e);
         }
     }
 
+    public void update(Region region) {
+        try(Connection connection = JDBCConnectionFactory.getInstance().getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_REGION_QUERY);
+            setRegionParams(region, preparedStatement);
+            preparedStatement.setLong(2, region.getId());
+
+            int res = preparedStatement.executeUpdate();
+            if(res == noChangedRows) {
+                LOGGER.error("Failed updating region");
+                throw new RegionSqlException("Failed updating region");
+            }
+            LOGGER.info("Region updated: {} rows affected", res);
+        } catch (SQLException e) {
+            LOGGER.error("Error updating region: {}", e.getMessage());
+            throw new RegionSqlException("Error updating region", e);
+        }
+    }
 }
