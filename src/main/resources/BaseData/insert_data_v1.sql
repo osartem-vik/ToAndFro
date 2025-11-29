75b0bd9cc9c631039c0efca837e0d0313df03846
stalgen <stalgen51@gmail.com>
2025-11-29 18:41:27 +0200

    English translation of insert_data_v1.sql file

--- src/main/resources/BaseData/insert_data_v1.sql
+++ src/main/resources/BaseData/insert_data_v1.sql
@@ -176,6 +176,7 @@ VALUES
 -- 26. Kyiv
 ('Kyiv', 9);
 
+
 -- 1) Ivan Petrenko — Passenger cars — Brovary
 INSERT INTO listing (user_id, category, city_id, title, description, price, created_at, status)
 VALUES ((SELECT id FROM user WHERE email = 'ivan.petrenko@gmail.com' LIMIT 1),
