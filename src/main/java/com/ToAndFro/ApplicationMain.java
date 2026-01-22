1693ebb5684765b7bf75310cef2c83ec56fba90d
zinchuknadia <nadiia.zinchuk.oi.2023@lpnu.ua>
2026-01-22 18:53:39 +0200

    Extract app run logic to AppRunner

--- src/main/java/com/ToAndFro/ApplicationMain.java
+++ src/main/java/com/ToAndFro/ApplicationMain.java
@@ -1,15 +1,11 @@
 package com.ToAndFro;
 
-import com.ToAndFro.client.CLIMenu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ApplicationMain {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(ApplicationMain.class, args);
-        CLIMenu menu = context.getBean(CLIMenu.class);
-        menu.run();
+        SpringApplication.run(ApplicationMain.class, args);
     }
 }
---
+++ src/main/java/com/ToAndFro/runner/AppRunner.java
@@ -0,0 +1,20 @@
+package com.ToAndFro.runner;
+
+import com.ToAndFro.client.CLIMenu;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements CommandLineRunner {
+
+    private CLIMenu menu;
+
+    public AppRunner(CLIMenu menu) {
+        this.menu = menu;
+    }
+
+    @Override
+    public void run(String... args) {
+        menu.run();
+    }
+}
