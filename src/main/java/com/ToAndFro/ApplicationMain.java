bd03d4a9d021f98e78435cef69c6b3a4c0e91a8d
zinchuknadia <nadiia.zinchuk.oi.2023@lpnu.ua>
2026-01-22 18:42:07 +0200

    Fix dependencies and make app run

--- src/main/java/com/ToAndFro/ApplicationMain.java
+++ src/main/java/com/ToAndFro/ApplicationMain.java
@@ -1,9 +1,15 @@
 package com.ToAndFro;
 
 import com.ToAndFro.client.CLIMenu;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+@SpringBootApplication
 public class ApplicationMain {
     public static void main(String[] args) {
-        new CLIMenu().run();
+        ConfigurableApplicationContext context = SpringApplication.run(ApplicationMain.class, args);
+        CLIMenu menu = context.getBean(CLIMenu.class);
+        menu.run();
     }
 }
