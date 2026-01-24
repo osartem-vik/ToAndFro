b739c3de172cd1db8ca9314287aabe7c0cbbcdf2
zinchuknadia <nadiia.zinchuk.oi.2023@lpnu.ua>
2026-01-24 15:52:31 +0200

    Refactor Listing to spring and add ListingRepository

--- src/main/java/com/ToAndFro/models/Listing.java
+++ src/main/java/com/ToAndFro/models/Listing.java
@@ -1,10 +1,13 @@
 package com.ToAndFro.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Type;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -14,14 +17,35 @@ import java.time.LocalDateTime;
 @EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "listing")
 public class Listing {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long userId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(columnDefinition = "enum('TRANSPORT', 'IMMOBILITY', 'ELECTRONICS', 'HOME', 'WORK', 'SERVICES', 'FASHION', 'ANIMALS')")
     private Category category;
-    private Long cityId;
+
+    @ManyToOne
+    @JoinColumn(name = "city_id")
+    private City city;
+
     private String title;
+
+    @Column(columnDefinition = "text")
     private String description;
+
     private BigDecimal price;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
     private LocalDateTime createdAt;
+
+    @Column(columnDefinition = "enum('ACTIVE', 'PENDING', 'SOLD', 'ARCHIVED')")
     private Status status;
 }
