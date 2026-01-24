0a0479b467223268e281f47d2fba76b4af3ae726
zinchuknadia <nadiia.zinchuk.oi.2023@lpnu.ua>
2026-01-24 15:53:51 +0200

    Remove unused imports

--- src/main/java/com/ToAndFro/models/Listing.java
+++ src/main/java/com/ToAndFro/models/Listing.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.Type;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
