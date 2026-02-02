0b7aa4f7c86ca26c1be0cbfb6f0d9d66d40cf116
Vladyslav <eddiedipper4228@gmail.com>
2026-02-03 00:59:49 +0100

    Changed imports in repositories to match new entities location

--- src/main/java/com/ToAndFro/repository/ChatRepository.java
+++ src/main/java/com/ToAndFro/repository/ChatRepository.java
@@ -1,6 +1,6 @@
 package com.ToAndFro.repository;
 
-import com.ToAndFro.models.Chat;
+import com.ToAndFro.models.entities.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/ToAndFro/repository/ImageRepository.java
+++ src/main/java/com/ToAndFro/repository/ImageRepository.java
@@ -1,6 +1,6 @@
 package com.ToAndFro.repository;
 
-import com.ToAndFro.models.Image;
+import com.ToAndFro.models.entities.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
